c8785eba56a3788267e02103566b27077431efd0
ciesielson <ciesielczyk.bartosz@hotmail.com>
2022-12-01 11:45:04 +0100

    setting the elements on main page with flex

--- app/page.tsx
+++ app/page.tsx
@@ -29,40 +29,62 @@ export default function Home() {
                 </nav>
             </header>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Hello <a href="http://mango-rewal.pl/">Mango!</a>
-                </h1>
-
-                <p className={styles.description}>
-                    Get started by editing <code className={styles.code}>app/page.tsx</code>
-                </p>
-
-                <div className={styles.grid}>
-                    <a href="https://beta.nextjs.org/docs" className={styles.card}>
-                        <h2>Documentation &rarr;</h2>
-                        <p>Find in-depth information about Next.js 13</p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/canary/examples"
-                        className={styles.card}
-                    >
-                        <h2>Examples &rarr;</h2>
-                        <p>Explore the Next.js 13 playground.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={styles.card}
-                    >
-                        <h2>Deploy &rarr;</h2>
-                        <p>Deploy your Next.js site to a public URL with Vercel.</p>
-                    </a>
+            <main className={styles.main2}>
+                <div className={styles.landing}>
+                    <h1>Dom Gościnny Mango</h1>
+                    <div>Rewal</div>
+                    <p>Zapraszamy do wypoczynku w Rewalu</p>
+                    <a href="https://google.com">O nas</a>
+                </div>
+                <div className={styles.overview}>
+                    <div className={styles.intro}>
+                        <div>Vivamus laoreet</div>
+                        <p>Lorem, ipsum.</p>
+                    </div>
+                    <div className={styles.subpages}>
+                        <div>Consectetur</div>
+                        <div>Malesuada</div>
+                        <div>Phasellus</div>
+                    </div>
                 </div>
             </main>
+
+            {/* <main className={styles.main}>
+                <div className={styles.landing}>
+                    <h1 className={styles.title}>Dom Gościnny Mango</h1>
+                    <h2>Domki Letnie Łokietek</h2>
+                    <h3>Zapraszamy do wypoczynku w Rewalu</h3>
+
+                    <p className={styles.description}>
+                        Get started by editing <code className={styles.code}>app/page.tsx</code>
+                    </p>
+
+                    <div className={styles.grid}>
+                        <a href="https://beta.nextjs.org/docs" className={styles.card}>
+                            <h2>Documentation &rarr;</h2>
+                            <p>Find in-depth information about Next.js 13</p>
+                        </a>
+
+                        <a
+                            href="https://github.com/vercel/next.js/tree/canary/examples"
+                            className={styles.card}
+                        >
+                            <h2>Examples &rarr;</h2>
+                            <p>Explore the Next.js 13 playground.</p>
+                        </a>
+
+                        <a
+                            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={styles.card}
+                        >
+                            <h2>Deploy &rarr;</h2>
+                            <p>Deploy your Next.js site to a public URL with Vercel.</p>
+                        </a>
+                    </div>
+                </div>
+            </main> */}
 
             <footer className={styles.footer}>
                 <a
